ccf547ce705ac6d6de754cb5b442990ba946cc50
Medeiros92 <thalesbehringer@gmail.com>
2021-09-14 13:06:34 -0300

    Recebendo dados HTML in script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//Recebendo dados do HTML
+const real = document.querySelector('#real');
+const dolar = document.querySelector('#dolar');
+const botao = document.querySelector('#btn');
+const input = document.querySelector('#input');
+const resultado = document.querySelector('#res');
+
