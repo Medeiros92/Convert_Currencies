fcd902905af6f315452cdc6132f7f7e121d8e0dd
Medeiros92 <thalesbehringer@gmail.com>
2021-09-14 15:54:50 -0300

    Funçoes finalizadas in script.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //Recebendo dados do HTML
-const real = document.querySelector('#real');
-const dolar = document.querySelector('#dolar');
+const real = document.getElementsByName('Real').value
+const dolar = document.getElementsByName('Dolar')
 const botao = document.querySelector('#btn');
 const input = document.querySelector('#input');
 const resultado = document.querySelector('#res');
 
+console.log(real);
+botao.addEventListener('click', function(){
+     getValRadio('opt')
+
+})
+
+function getValRadio(name){
+const real = document.getElementsByName(name)
+    
+    for (let i = 0; i < real.length; i++){
+        if(real[i].checked){
+             var val = real[i].value
+        }if(val == '1'){
+            const price = parseFloat(input.value) / 5
+            return resultado.innerHTML = `Valor Convertido: R$${price.toFixed(2)}`
+        }else{
+            const price = parseFloat(input.value) * 5
+            return resultado.innerHTML = `Valor Convertido: R$${price.toFixed(2)}`
+        }
+    }
+}
